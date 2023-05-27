74c086c20856db5ae45670fe0c63ab7004642542
shubhampande9960 <shubhampande9960@gmail.com>
2023-05-27 10:38:50 +0530

    Add files via upload

---
+++ razorpay/index.php
@@ -0,0 +1,9 @@
+<html>
+<form>
+<script src = "https://cdn.razorpay.com/static/widget/payment-button.js"
+data-payment_button_id = "pl_LatgEd6cmU5N1L"
+data-button_text = "Buy Now"
+data-button_theme = "brand-color">
+</script>
+</form>
+</html>
