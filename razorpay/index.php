2d964498c27e3031fc0c935bfb3f3d92af5a1e12
shubhampande9960 <shubhampande9960@gmail.com>
2025-07-03 11:05:43 +0530

    Delete razorpay/index.php

--- razorpay/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<form>
-<script src = "https://cdn.razorpay.com/static/widget/payment-button.js"
-data-payment_button_id = "pl_LatgEd6cmU5N1L"
-data-button_text = "Buy Now"
-data-button_theme = "brand-color">
-</script>
-</form>
-</html>
